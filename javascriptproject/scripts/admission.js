8f2323e921a06d2633feb61d7a9da2fb9e055356
cprincec <chukwuprincec@gmail.com>
2022-06-03 21:57:40 +0100

    commit

--- javascriptproject/scripts/admission.js
+++ javascriptproject/scripts/admission.js
@@ -1,29 +1,46 @@
-// const aStudent = {};
+//url for student database
+const url = "https://cprincec.github.io/javascriptproject/javascriptproject/students.json";
 
-// function createStudent() {
-//     let lastName = document.querySelector("#lname").value;
-//     let firstName = document.querySelector("#fname").value;
-//     let middleName = document.querySelector("#mname").value;
-//     let dateOfBirth = document.querySelector("#dob").value;
-//     let gender = document.querySelector("#gender").value;
-//     let address = document.querySelector("#raddress").value;
-//     let nationality = document.querySelector("#nationality").value;
-//     let stateOfOrigin = document.querySelector("#sorigin").value;
-//     let lga = document.querySelector("#lga").value;
-//     let sClass = document.querySelector("#class").value;
+// student database object variable
+var students;
+var student = {};
 
-//     aStudent.studentName = firstName + " " + middleName + " " + lastName
-//     aStudent.studentDOB = dateOfBirth
-//     aStudent.studentGender = gender
-//     aStudent.studentAddress = address
-//     aStudent.studentNationality = nationality
-//     aStudent.studentState = stateOfOrigin
-//     aStudent.studentLga = lga
-//     aStudent.studentClass = sClass
-//     aStudent.studentNumber = students.length
+
+//generate student object
+async function getstudent(url) {
+    let response = await fetch(url);
+    students = await response.json();
+};
+
+// Create new student object in student database
+// using information entered in registration form.
+function createStudent() {
+    let lastName = document.querySelector("#lname").value;
+    let firstName = document.querySelector("#fname").value;
+    let middleName = document.querySelector("#mname").value;
+    let dateOfBirth = document.querySelector("#dob").value;
+    let gender = document.querySelector("#gender").value;
+    let address = document.querySelector("#raddress").value;
+    let nationality = document.querySelector("#nationality").value;
+    let stateOfOrigin = document.querySelector("#sorigin").value;
+    let lga = document.querySelector("#lga").value;
+    let sClass = document.querySelector("#class").value;
+
+    student.firstName = firstName;
+    student.middleName = middleName;
+    student.lastName = lastName;
+    student.dateOfBirth = dateOfBirth
+    student.gender = gender;
+    student.Address = address;
+    student.nationality = nationality;
+    student.state = stateOfOrigin;
+    student.lga = lga;
+    student.class = sClass;
+    student.studentNumber = Math.random();
     
-//     createProfile(aStudent)
-// }
+};
+
+
 
 
 // function createProfile()    {
@@ -39,7 +56,9 @@
 
 // }
 
-// document.querySelector(".submit").addEventListener("click", createStudent);
-// console.log(students);
+createStudent()
+getstudent(url);
+console.log(students)
 
-const url = 
+
+
